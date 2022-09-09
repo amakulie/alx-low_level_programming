2aa7f40b5ad0bdb12cc1ad1e7d1dcd66fd6fcf13
amakulie <technologycsolutions@gmail.com>
2022-09-09 02:17:15 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Description using the main function
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
- */
+*/
 int main(void)
 {
- puts("\"programming is like building a multilingual puzzle");
- return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
