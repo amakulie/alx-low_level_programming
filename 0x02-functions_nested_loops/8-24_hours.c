02811be627b0f2589a0c10ae72a6ba7fda93cf91
amakulie <technologycsolutions@gmail.com>
2022-09-15 12:23:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ while (hours <= 23)
 while (minutes <= 59)
 {
 mins_remainder = minutes % 10;
-hours_reminder = hours % 10;
+hours_remainder = hours % 10;
 _putchar (hours / 10 + '0');
 _putchar (hours_remainder + '0');
 _putchar (':');
