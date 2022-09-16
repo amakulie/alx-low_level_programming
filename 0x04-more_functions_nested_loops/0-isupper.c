c129423af30d499d14252264b0e9ce42eabfa916
amakulie <technologycsolutions@gmail.com>
2022-09-16 10:45:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper -checks if a character is uppercas or not
+ * @c: chracter to be tested
+ * Return: 1 whether it is, 0 otherwise
+ */
+
+int_isupper(intc)
+{
+if ((c >= 'A') && (c <= 'Z'))
+{
+return (1);
+}
+return (0);
+}
