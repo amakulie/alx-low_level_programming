5cf1bcedba2ca2ebbf929d93fbe9f05f41c501cb
amakulie <technologycsolutions@gmail.com>
2022-09-16 12:15:38 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
- * main - header file for protoypes
- * void protoypes (void)
- * int protoypes (int)
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
  */
 
 void print_numbers(void);
@@ -15,9 +15,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int_putchar(char);
-int_isupper(int);
-int_isdigit(int);
-int mul (int, int);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
 #endif /* MAIN_H */
