db61c6d56dd44611b1bc608890135f38e5a7781c
amakulie <technologycsolutions@gmail.com>
2022-09-15 18:54:19 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ if (z > 99)
 _putchar(',');
 _putchar(32);
 _putchar((z / 100) + '0');
-_putchar(((z % 10) % 10) + '0');
+_putchar(((z / 10) % 10) + '0');
 _putchar((z % 10) + '0');
 }
 else if (z > 9)
