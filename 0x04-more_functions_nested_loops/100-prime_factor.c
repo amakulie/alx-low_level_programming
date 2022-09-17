d7fe36ebbd9c4acb77cf79c1607a6e638b0fae6e
amakulie <technologycsolutions@gmail.com>
2022-09-16 19:14:07 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,7 +4,7 @@
  * print_number -prints an integer
  * @n: randon integer
  * *Return: void
- * */
+ */
 
 void print_number(int n)
 {
@@ -15,8 +15,8 @@ int ct = 0;
 if (n < 0)
 {
 	_putchar ('-');
-	ld = (char)('0' -(n % 10));
-	n = n / 10;
+	ld = (char)('0' - (n % 10));
+	n = n / -10;
 }
 
 else
@@ -28,16 +28,16 @@ n = n / 10;
 r = 0;
 while (n > 0)
 {
-	r =r * 10 + (n % 10);
+	r = r * 10 + (n % 10);
 	n = n / 10;
-	ct++
+	ct++;
 }
 
 while (r > 0)
 {
 cr = (char)((r % 10 + '0');
 _putchar(cr);
-r =r /10
+r =r / 10
 ct--
 }
 while (ct != 0)
