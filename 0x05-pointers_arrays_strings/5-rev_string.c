b0cabfdc64a4116f91524f6b6b5410c599555098
amakulie <technologycsolutions@gmail.com>
2022-09-19 14:54:36 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -26,12 +26,12 @@ tmp = *(s + i);
  * Returns: the length of the given string
  */
 
-int_strlen(char *s)
+int _strlen(char *s)
 {
 int len = 0;
 
 while (*(s + len) != '\0')
-len++
+len++;
 
 return (len);
 }
