94c6b90d05e294e1f3f53632dc120dbb2cea0f74
amakulie <technologycsolutions@gmail.com>
2022-09-22 08:21:48 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+
 int index = 0, dest_len = 0;
 
 while (dest[index++])
@@ -19,5 +19,4 @@ for (index = 0; src[index] && index < n; index++)
 	dest[dest_len++] = src[index];
 
 return (dest);
-
 }
