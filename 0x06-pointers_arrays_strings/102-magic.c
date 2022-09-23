2ce54529af07ac09821edcd4e7765b3485dd0c0a
amakulie <technologycsolutions@gmail.com>
2022-09-22 17:46:26 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
+
+
+/**
+ * main- Entry point
+ * Decscription: adds a line in the code and uses printf
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -18,7 +25,7 @@ p = &n;
  *   - you are not allowed to code anything else than this line of code
  */
 
-p[5] = 98;
+p[5]	= 98;
 
 /* ...so that this prints 98\n */
 
