d771b7b18e2e571cab7e03085357ffc21efcb1a5
amakulie <technologycsolutions@gmail.com>
2022-09-27 13:31:21 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
- * accept: string to check against
+ * @accept: string to check against
  *
  * Return: number of bytes of s in accept
  */
