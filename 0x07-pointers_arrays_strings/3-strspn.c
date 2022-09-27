cf7b803cc3f0ec861642c3f59eea13bac4193268
amakulie <technologycsolutions@gmail.com>
2022-09-27 13:20:04 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _ strspn - gets lengthof a prefix substring
+ * _istrspn - gets lengthof a prefix substring
  * @s: string to check
  * accept: string to check against
  *
@@ -14,7 +14,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == accept[i])
+		for (j =0; accept[j]; j++)
+			if (s[i] == accept[i])
 			break;
 	}
 	if (!accept[i])
