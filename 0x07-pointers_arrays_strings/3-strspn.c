787216ef7a147e08f3cbb3537cfee8b855e6d425
amakulie <technologycsolutions@gmail.com>
2022-09-27 16:47:54 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,10 +14,13 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
+		{
 
 			if (s[i] == accept[j])
 			break;
-	}
+		}
 	if (!accept[j])
 		break;
-} 
+	}
+return (i);
+}
