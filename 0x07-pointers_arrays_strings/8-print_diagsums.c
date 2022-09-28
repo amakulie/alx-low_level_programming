03eb894da4ec74d951a3aacd8b0cbc2c4e65e8ba
amakulie <technologycsolutions@gmail.com>
2022-09-27 18:09:18 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,7 @@ void print_diagsums(int *a, int size)
 		p = (i * size) + i;
 		l += *(a + p);
 	}
-	for (j = 0, j < size; j++)
+	for (j = 0; j < size; j++)
 	{
 		p = (j * size) + (size - 1 - j);
 		r += *(a + p);
